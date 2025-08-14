8e5a0212addba5691eb9b1bdd891636fab7f64d1
Anyeloo <Anyelooh13@gmail.com>
2025-08-14 01:21:28 -0400

    Add files via upload

---
+++ proyecto.py
@@ -0,0 +1,8 @@
+nombre='anyelo'
+print(nombre)
+#las letras y palabras son tipo de datos str (strings
+#los str simepre van entre comillas
+edad = 18
+#los numero son tipos de datos int (enteros)
+edad = edad + 1
+print('hola soy anyelo') if[5+2=7]
